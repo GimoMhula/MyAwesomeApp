26d1f0d611790e1f7558f596f7cb6751b95dd599
pascoalb <pascoal.bd@gmail.com>
2021-05-20 03:00:19 +0100

    added constants class

---
+++ app/src/main/java/dev/visum/demoapp/utils/Constants.java
@@ -0,0 +1,20 @@
+package dev.visum.demoapp.utils;
+
+public class Constants {
+    private static final Constants ourInstance = new Constants();
+
+    public static Constants getInstance() {
+        return ourInstance;
+    }
+
+    private Constants() {
+    }
+
+    // REGEX
+    public String PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$";
+    public String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+
+    // SharedPreferences
+    public String SP_NAME = "MyApp";
+    public String SP_TOKEN = "accessToken";
+}
