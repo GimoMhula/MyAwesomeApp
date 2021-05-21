9df404ab224e5c702f470c935d4f919bc09896c0
pascoalb <pascoal.bd@gmail.com>
2021-05-21 01:19:16 +0100

    added products list endpoint

--- app/src/main/java/dev/visum/demoapp/data/api/GetDataService.java
+++ app/src/main/java/dev/visum/demoapp/data/api/GetDataService.java
@@ -2,9 +2,14 @@ package dev.visum.demoapp.data.api;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
+import dev.visum.demoapp.model.ProductResponseModel;
 import dev.visum.demoapp.model.ResponseModel;
 import dev.visum.demoapp.model.UserAgentResponseModel;
 import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
@@ -12,4 +17,7 @@ public interface GetDataService {
 
     @POST("login")
     Call<ResponseModel<UserAgentResponseModel>> loginUser(@NotNull @Query("email") String email, @NotNull @Query("password") String password);
+
+    @GET("product")
+    Call<ResponseModel<List<ProductResponseModel>>> getProductsList(@Header("Authorization") String authorization);
 }
