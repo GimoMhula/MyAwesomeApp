a63a8738b6c6c9977059bb6b0f37f7bbf3562991
pascoalb <pascoal.bd@gmail.com>
2021-05-21 01:19:02 +0100

    added flow to products list

--- app/src/main/java/dev/visum/demoapp/activity/MainActivity.java
+++ app/src/main/java/dev/visum/demoapp/activity/MainActivity.java
@@ -1,23 +1,27 @@
 package dev.visum.demoapp.activity;
 
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import dev.visum.demoapp.R;
-import dev.visum.demoapp.adapter.AdapterGridItemCategory;
-import dev.visum.demoapp.data.DataGenerator;
-import dev.visum.demoapp.model.BaseActivity;
-import dev.visum.demoapp.model.ItemCategory;
-import dev.visum.demoapp.utils.Tools;
-import dev.visum.demoapp.widget.SpacingItemDecoration;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.List;
+
+import dev.visum.demoapp.R;
+import dev.visum.demoapp.adapter.AdapterGridItemCategory;
+import dev.visum.demoapp.data.DataGenerator;
+import dev.visum.demoapp.fragment.ProductGridFragment;
+import dev.visum.demoapp.model.BaseActivity;
+import dev.visum.demoapp.model.ItemCategory;
+import dev.visum.demoapp.utils.Tools;
+import dev.visum.demoapp.widget.SpacingItemDecoration;
 
 public class MainActivity extends BaseActivity {
 
@@ -41,6 +45,11 @@ public class MainActivity extends BaseActivity {
 
 
         List<ItemCategory> items = DataGenerator.getItemCategory(this);
+        ItemCategory itemCategory = new ItemCategory();
+        itemCategory.image = R.drawable.ic_image_black_24dp;
+        itemCategory.title = "Produtos";
+        itemCategory.imageDrw = AppCompatResources.getDrawable(this, itemCategory.image);
+        items.add(itemCategory);
         //set data and list dev.visum.demoapp.adapter
         mAdapter = new AdapterGridItemCategory(this, items);
         recyclerView.setAdapter(mAdapter);
@@ -49,11 +58,19 @@ public class MainActivity extends BaseActivity {
         mAdapter.setOnItemClickListener(new AdapterGridItemCategory.OnItemClickListener() {
             @Override
             public void onItemClick(View view, ItemCategory obj, int position) {
-               // Toast.makeText(MainActivity.this,  "Item " + obj.title + " clicked", Toast.LENGTH_SHORT).show();
-                Bundle bundle = new Bundle();
-                bundle.putString("content",obj.title);
+               Toast.makeText(MainActivity.this,  "Item " + obj.title + " clicked", Toast.LENGTH_SHORT).show();
 
-                startActivity(ContentActivity.class,bundle,null);
+               if (obj.title.equals("Produtos")) {
+                   FragmentTransaction fragmentTransaction = getInstance().getSupportFragmentManager().beginTransaction();
+                   fragmentTransaction.add(R.id.parent_view, new ProductGridFragment());
+                   fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                   fragmentTransaction.addToBackStack(null);
+                   fragmentTransaction.commit();
+               } else {
+                   Bundle bundle = new Bundle();
+                   bundle.putString("content",obj.title);
+                   startActivity(ContentActivity.class,bundle,null);
+               }
 
             }
         });
