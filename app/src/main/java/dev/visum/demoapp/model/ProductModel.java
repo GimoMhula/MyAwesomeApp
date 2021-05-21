962d01f1e8e84df787fcb341d1b5cc154d245bcf
pascoalb <pascoal.bd@gmail.com>
2021-05-21 01:20:06 +0100

    added product model

---
+++ app/src/main/java/dev/visum/demoapp/model/ProductModel.java
@@ -0,0 +1,28 @@
+package dev.visum.demoapp.model;
+
+import android.graphics.drawable.Drawable;
+
+public class ProductModel {
+
+    public int image;
+    public Drawable imageDrw;
+    public String title;
+    public String price;
+    public String url;
+
+    public ProductModel(int image, Drawable imageDrw, String title, String price, String url) {
+        this.image = image;
+        this.imageDrw = imageDrw;
+        this.title = title;
+        this.price = price;
+        this.url = url;
+    }
+
+    public ProductModel(String title, String price, String url) {
+        this.image = 0;
+        this.title = title;
+        this.price = price;
+        this.url = url;
+        this.imageDrw = null;
+    }
+}
